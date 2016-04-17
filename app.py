4a0f46e03f4c18b22c4607a457d029b24ef5a592
asimonia <alex.simonian@gmail.com>
2016-04-17 13:34:06 -0500

    added debugging and render template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,4 +7,4 @@ def hello_method():
 	return render_template('login.html')
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
